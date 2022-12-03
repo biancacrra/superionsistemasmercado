bd483084c6dd5f5696c404567384d4b460399daa
Bianca Correa <100317868+biancacrra@users.noreply.github.com>
2022-12-03 20:02:46 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,39 @@
-<?php 
-$server = "localhost";
-$user = "postgres";
-$password = "123456";
-$port = "5432";
+<?php
+session_start();
 
-$infos_conexao = new conexao ("host=$server port=$port dbname=mercadosystem user=$user password=$password");
+include ('database.php');
+if(isset($_POST['email']) || isset($_POST['senha'])) {
 
+    if($_POST['email'] == ''){
+        echo "preencha o email";
+    }else if ($_POST['senha'] == ''){
+        echo "senha inválida";
+    }else {
 
-$conexao = pg_connect($infos_conexao);
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
 
+        $sql_code = 
+        "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Deu erro no sql: " . $mysqli->error);
 
+        $qtdequery = $sql_query->num_rows;
+        $usuario = $sql_query->fetch_assoc();
+        if($qtdequery == 1){
+            if(!isset($_SESSION)){
+                session_start();
+            }
+            $_SESSION['logado'] = true;
+            $_SESSION['id'] = $usuario['id'];
+            header("location: homepageadm.php");
+
+        }else {
+            echo "login ou senha inválidos";
+            header("location: index.php");
+        }
+
+    }
+
+}
 
 ?>
