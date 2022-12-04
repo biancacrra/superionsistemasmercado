38bca6105bf5c1a110e2f6fd3e04c1bcebeb668b
Bianca Correa <100317868+biancacrra@users.noreply.github.com>
2022-12-04 13:14:03 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,39 +1,60 @@
 <?php
 session_start();
+include('cadastro.php');
+include('database.php');
 
-include ('database.php');
-if(isset($_POST['email']) || isset($_POST['senha'])) {
 
-    if($_POST['email'] == ''){
+if (isset($_POST['email']) || isset($_POST['senha'])) {
+    if ($_POST['email'] == '') {
         echo "preencha o email";
-    }else if ($_POST['senha'] == ''){
+    } else if ($_POST['senha'] == '') {
         echo "senha inválida";
-    }else {
+    } else {
 
         $email = $mysqli->real_escape_string($_POST['email']);
         $senha = $mysqli->real_escape_string($_POST['senha']);
 
-        $sql_code = 
-        "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
-        $sql_query = $mysqli->query($sql_code) or die("Deu erro no sql: " . $mysqli->error);
+        $senha = md5($senha);
 
-        $qtdequery = $sql_query->num_rows;
-        $usuario = $sql_query->fetch_assoc();
-        if($qtdequery == 1){
-            if(!isset($_SESSION)){
-                session_start();
+        $cargoadm = 1;
+        $cargocaixa = 2;
+
+        if (
+            $sql_code =
+            "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha' AND cargo = '$cargoadm'"
+        ) {
+            $sql_query = $mysqli->query($sql_code) or die("Deu erro no sql: " . $mysqli->error);
+            $qtdequery = $sql_query->num_rows;
+            $usuario = $sql_query->fetch_assoc();
+            if ($qtdequery == 1) {
+                if (!isset($_SESSION)) {
+                    session_start();
+                }
+                $_SESSION['admlogado'] = true;
+                $_SESSION['id'] = $usuario['id'];
+                header("location: homepageadm.php");
+            } else {
+                echo "login ou senha inválidos";
+                header("location: index.php");
             }
-            $_SESSION['logado'] = true;
-            $_SESSION['id'] = $usuario['id'];
-            header("location: homepageadm.php");
-
-        }else {
-            echo "login ou senha inválidos";
-            header("location: index.php");
         }
-
+        if ($sql_code =
+            "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha' AND cargo = '$cargocaixa'"
+        ) {
+            $sql_query = $mysqli->query($sql_code) or die("Deu erro no sql: " . $mysqli->error);
+            $qtdequery = $sql_query->num_rows;
+            $usuario = $sql_query->fetch_assoc();
+            if ($qtdequery == 1) {
+                if (!isset($_SESSION)) {
+                    session_start();
+                }
+                $_SESSION['caixalogado'] = true;
+                $_SESSION['id'] = $usuario['id'];
+                header("location: homepagecaixa.php");
+            } else {
+                echo "login ou senha inválidos";
+                header("location: index.php");
+            }
+        }
     }
-
 }
-
-?>
