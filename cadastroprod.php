011e59c2288af93f0d363bcc6c584bb86bef8ae6
Bianca Correa <100317868+biancacrra@users.noreply.github.com>
2022-12-04 13:10:20 -0300

    Update cadastroprod.php

--- cadastroprod.php
+++ cadastroprod.php
@@ -1,15 +1,18 @@
 <?php
+include('database.php');
 session_start();
 
-if(!isset($_SESSION['logado'])){
+if (!isset($_SESSION['admlogado'])) {
     header("location:index.php");
     session_destroy();
 }
 
-if(isset($_GET['logout'])){
+if (isset($_GET['logout'])) {
     header("location:index.php");
     session_destroy();
 }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -30,7 +33,7 @@ if(isset($_GET['logout'])){
 
     <section class="conteudo">
         <div class="cadastro-prod">
-            <form class="form form-cadastro">
+            <form method="POST" class="form form-cadastro">
                 <div><a href="homepageadm.php"><img class="cancelicon" src="imgs/cancel.png"></a></div>
                 <h3 class="titulo">Cadastrar novo produto:</h3>
                 <label class="titulos-cadastro" for="nome-prod">Nome do produto:</label>
@@ -38,18 +41,67 @@ if(isset($_GET['logout'])){
                 <label class="titulos-cadastro" for="descricao-prod">Descrição:</label>
                 <textarea name="descricao-prod" id="descricao-prod" class="campo" placeholder="Digite uma breve descrição do produto."></textarea>
                 <label class="titulos-cadastro" for="categoria-opcoes">Categoria:</label>
-                <select class="campo" id="categoria-opcoes" name="categoria-opcoes" required>
-                    <!--fazer em um arquivo php e fazer um foreach das categorias-->
-                </select>
+
+                <?php
+                $sql = "SELECT * FROM categoria";
+                if ($query = mysqli_query($mysqli, $sql)) {
+                    $id_categoria = [];
+                    $categoria = [];
+                    $x = 0;
+                    while ($registros = mysqli_fetch_assoc($query)) {
+                        $id_categoria[$x] = $registros['ID_categoria'];
+                        $categoria[$x] = $registros['nome_categoria'];
+                        $x++;
+                    }
+
+
+                    echo "<select class='campo' id='categoria-opcoes' name='categoria-opcao'>";
+                    $opcao = $_POST['categoria-opcao'];
+
+                    for ($i = 0; $i < $x; $i++) {
+                        echo "<option name='selecionado'>";
+                        echo $id_categoria[$i]. " - ";
+                        echo $categoria[$i];
+                        echo "</option>";
+                    }
+
+                    echo "</select>";
+                }
+
+                $arrayOpcao = explode(' - ',$opcao);
+                echo "<br>";
+                $id = $arrayOpcao[0];
+
+
+
+                
+                ?>
+
                 <div id="cadastro-cat"><a class="btn" href="cadastrocat.php">Cadastrar nova Categoria</a></div>
                 <label class="titulos-cadastro" for="valor-prod">Valor de venda:</label>
-                <input id="valor-prod" name="valor-prod" class="campo" type="number" placeholder="0,00" required>
-                <input class="btn btn-cadastro" type="submit" value="Cadastrar novo produto">
+                <input id="valor-prod" name="valor-prod" class="campo" type="text" placeholder="0.00" required>
+                <input class="btn btn-cadastro" name="cadastrar" type="submit" value="Cadastrar novo produto">
             </form>
 
+            <?php
+            if (isset($_POST['cadastrar'])) {
+                $nome = $_POST['nome-prod'];
+                $descricao = $_POST['descricao-prod'];
+                $valor = $_POST['valor-prod'];
+
+                $query = mysqli_query(
+                    $mysqli,
+                    "INSERT INTO produto (nome, descricao, preco, fk_ID_categoria) 
+                    VALUES ('$nome', '$descricao', '$valor','$id') "
+                );
+            }
+
+            ?>
 
         </div>
     </section>
+    <div class="rodape"></div>
+
 
 </body>
 
