667ce4ec9043ed7127c490c833c66621d95d0403
Bianca Correa <100317868+biancacrra@users.noreply.github.com>
2022-12-04 13:12:01 -0300

    Update mercadosystem.sql

--- mercadosystem.sql
+++ mercadosystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 02-Dez-2022 às 09:33
+-- Tempo de geração: 04-Dez-2022 às 17:11
 -- Versão do servidor: 10.4.25-MariaDB
 -- versão do PHP: 8.1.10
 
@@ -30,8 +30,21 @@ SET time_zone = "+00:00";
 CREATE TABLE `categoria` (
   `ID_categoria` int(11) NOT NULL,
   `nome_categoria` varchar(240) NOT NULL,
+  `descricao` text NOT NULL,
   `qtde_categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `categoria`
+--
+
+INSERT INTO `categoria` (`ID_categoria`, `nome_categoria`, `descricao`, `qtde_categoria`) VALUES
+(1, 'Hortifruti', 'Frutas, verduras, legumes e diversos produtos frescos.', 0),
+(3, 'Açougue', 'Carnes bovinas, suínas, embutidos, etc.', 0),
+(4, 'Massas', 'Tipos de macarrão, lasanha, pastéis, etc.', 0),
+(5, 'Padaria', 'Pães, doces e bolos.', 0),
+(6, 'Bebidas', 'Alcoólicos, águas, refrigerantes, sucos, etc.', 0),
+(7, 'Grãos', 'Tipos de feijão, milho, oleaginosas, etc', 0);
 
 -- --------------------------------------------------------
 
@@ -43,9 +56,19 @@ CREATE TABLE `produto` (
   `ID_produto` int(11) NOT NULL,
   `nome` varchar(200) NOT NULL,
   `descricao` text NOT NULL,
-  `preco` float NOT NULL,
-  `estoque` int(11) NOT NULL DEFAULT 0
+  `preco` decimal(10,2) NOT NULL,
+  `estoque` int(11) NOT NULL DEFAULT 0,
+  `fk_ID_categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`ID_produto`, `nome`, `descricao`, `preco`, `estoque`, `fk_ID_categoria`) VALUES
+(242, 'Cenoura', 'Laranjinha', '2.99', 0, 1),
+(243, 'Filé de alcatra', 'Macio e suculento.', '32.99', 0, 3),
+(244, 'Arroz branco', 'Arroz soltinho, vamos almoçarr?', '4.75', 0, 7);
 
 -- --------------------------------------------------------
 
@@ -54,14 +77,22 @@ CREATE TABLE `produto` (
 --
 
 CREATE TABLE `usuario` (
-  `ID_usuario` int(11) NOT NULL,
-  `login` varchar(200) NOT NULL,
-  `senha` char(100) NOT NULL,
+  `id` int(11) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `senha` char(255) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `sobrenome` varchar(100) NOT NULL,
-  `cargo` int(11) NOT NULL,
-  `cpf` int(11) NOT NULL
+  `cargo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `email`, `senha`, `nome`, `sobrenome`, `cargo`) VALUES
+(13, 'joanadacosta@gmail.com', '202cb962ac59075b964b07152d234b70', 'Joana', 'da Costa', 2),
+(14, 'cleberasilva@gmail.com', '202cb962ac59075b964b07152d234b70', 'Cleber', 'Augusto da Silva', 1),
+(16, 'joelsinho02@gmail.com', '202cb962ac59075b964b07152d234b70', 'Joelson', 'Pereira', 2);
 
 --
 -- Índices para tabelas despejadas
@@ -77,13 +108,14 @@ ALTER TABLE `categoria`
 -- Índices para tabela `produto`
 --
 ALTER TABLE `produto`
-  ADD PRIMARY KEY (`ID_produto`);
+  ADD PRIMARY KEY (`ID_produto`),
+  ADD KEY `fk_ID_categoria` (`fk_ID_categoria`);
 
 --
 -- Índices para tabela `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`ID_usuario`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -93,19 +125,30 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `ID_categoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `ID_produto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=246;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `ID_usuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD CONSTRAINT `fk_ID_categoria` FOREIGN KEY (`fk_ID_categoria`) REFERENCES `categoria` (`ID_categoria`),
+  ADD CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`fk_ID_categoria`) REFERENCES `categoria` (`ID_categoria`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
