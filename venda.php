dda6c18c15af7e8ad2d2f8f5a908276b3e936146
Bianca Correa <100317868+biancacrra@users.noreply.github.com>
2022-12-07 12:05:55 -0300

    Update venda.php

--- venda.php
+++ venda.php
@@ -14,19 +14,39 @@ if (isset($_GET['logout'])) {
     session_destroy();
 }
 
-
 $sql = "SELECT * FROM produto ORDER BY nome";
 $resultado = $mysqli->query($sql);
+$x = 0;
 
+while ($dados_prod = mysqli_fetch_assoc($resultado)) {
 
-$sql2 = "SELECT * FROM categoria";
-$resultadoid = $mysqli->query($sql2);
+    $id = $dados_prod['ID_produto'];
+    $nome = $dados_prod['nome'];
+    $descricao = $dados_prod['descricao'];
+    $valor = $dados_prod['preco'];
 
-$sql3 = "SELECT COUNT(*) FROM produto";
-$qtde_itens = $mysqli->query($sql3);
+    $arrayprodid[$x] = $id;
+    $arrayprodnome[$x] = $nome;
+    $arrayproddesc[$x] = $descricao;
+    $arrayprodvalor[$x] = $valor;
+    $x++;
+}
 
+$totalitens = $x;
 
+for ($y = 0; $y < $totalitens; $y++) {
 
+    $arrayprodutos = [
+
+        'id' => $arrayprodid[$y],
+        'produto' => $arrayprodnome[$y],
+        'descricao' => $arrayproddesc[$y],
+        'valor' => $arrayprodvalor[$y]
+
+    ];
+}
+$totalprod = $y;
+$quantidade = 0;
 ?>
 
 <!DOCTYPE html>
@@ -46,64 +66,44 @@ $qtde_itens = $mysqli->query($sql3);
     <img class="logo" src="imgs/logosistema.png">
 
     <section class="conteudo">
-        <div class="estoque-block">
+        <div class="estoque-block venda-block">
             <div>
-                <?php 
-                if(!isset($_SESSION['admlogado'])){
+                <?php
+                if (!isset($_SESSION['admlogado'])) {
                     echo "<a href='homepagecaixa.php'><img class='canceliconestoque' src='imgs/cancel.png'></a>";
                 }
-                if (!isset($_SESSION['caixalogado'])){
+                if (!isset($_SESSION['caixalogado'])) {
                     echo "<a href='homepageadm.php'><img class='canceliconestoque' src='imgs/cancel.png'></a>";
                 }
-                    ?>
-                </div>
-
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Descrição</th>
-                        <th scope="col">Valor</th>
-                        <th scope="col">Estoque</th>
-                        <th scope="col">Seleção</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    <?php
-                    while ($dados_prod = mysqli_fetch_assoc($resultado)) {
-                        echo "<tr>";
-                        echo "<td>" . $dados_prod['nome'] . "</td>";
-                        echo "<td>" . $dados_prod['descricao'] . "</td>";
-                        echo "<td>" . $dados_prod['preco'] . "</td>";
-                        echo "<td>" . $dados_prod['estoque'] . "</td>";
-                        echo "<td><input type='checkbox' name='prod-selecionado'></td>";
-                        echo "</tr>";
+                ?>
+            </div>
+                <div class="formulario-block">
+                <form action="venda2.php" method="POST">
+            <?php
+            foreach ($arrayprodnome as $chave => $produto) {
+                echo "<label class='form-check-label labelvenda'>";
+                    $id = $arrayprodid[$chave];
+                    echo "$produto  -  $arrayprodvalor[$chave]
+                        <input class='form-check-input' type='checkbox' name='prodescolhido[$id]' value='$arrayprodvalor[$chave]'><br><br>
+                        <label>";
                     }
+            ?>
+            <div class="botoescompra">
+                <?php 
+                if (!isset($_SESSION['admlogado'])) {
+                    echo "<a href='homepagecaixa.php'><button type='button' class='btn btn-danger' name='cancelar'>Cancelar</button></a>";
+                }
+                if (!isset($_SESSION['caixalogado'])) {
+                    echo "<a href='homepageadm.php'><button type='button' class='btn btn-danger' name='cancelar'>Cancelar</button></a>";
+                }
+                ?>
 
-                    ?>
-                </tbody>
-            </table>
+                <button type="submit" class="btn btn-success"><input type="submit" value="" hidden> Prosseguir</button>
+            </div>
+            </form>
+            </div>
         </div>
     </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 </body>
 
